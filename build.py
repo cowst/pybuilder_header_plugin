8c8cd475d0d9636bbfe8753cb1e44ebe3198dbad
aelgru <aelgru@gmail.com>
2014-10-04 20:29:11 +0200

    Extracted header to separate file.

--- build.py
+++ build.py
@@ -47,21 +47,7 @@ def set_properties(project):
     project.set_property('flake8_break_build', True)
 
     project.set_property('pybuilder_header_plugin_break_build', True)
-    project.set_property('pybuilder_header_plugin_expected_header', """#   pybuilder_header_plugin
-#   Copyright 2014 Michael Gruber
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-""")
+    project.set_property('pybuilder_header_plugin_expected_header', open('header.py').read())
 
     project.get_property('distutils_commands').append('bdist_wheel')
     project.set_property('distutils_classifiers', [
